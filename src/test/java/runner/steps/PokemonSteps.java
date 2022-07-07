fb15fd112af8ff26a66dcf98d7fc56d8810dc885
Marcos Couto <marcosmartinellicouto@gmail.com>
2022-07-06 23:54:03 -0300

    refactor: improve code

--- src/test/java/runner/steps/PokemonSteps.java
+++ src/test/java/runner/steps/PokemonSteps.java
@@ -1,13 +1,12 @@
 package runner.steps;
 
-import io.cucumber.java.Before;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
 import io.restassured.RestAssured;
-import io.restassured.parsing.Parser;
 import io.restassured.response.Response;
-import org.hamcrest.Matchers;
+
+import static org.hamcrest.Matchers.equalTo;
 
 public class PokemonSteps {
 
@@ -15,19 +14,19 @@ public class PokemonSteps {
     private Response response;
 
     @Dado("que escolho o pokemon {string}")
-    public void escolhePokemon(String pokemon){
+    public void escolherPokemon(String pokemon) {
         this.pokemon = pokemon;
     }
 
     @Quando("faço a busca na api de pokemons")
-    public void fazerBuscarNaApi(){
+    public void fazerBuscarNaApi() {
         this.response = RestAssured
-                .get("https://pokeapi.co/api/v2/pokemon/"+pokemon);
+                .get("https://pokeapi.co/api/v2/pokemon/" + pokemon);
     }
 
     @Então("verifico se seu id é o {int}")
-    public void receberInformacoes(int id){
-        this.response.then().body("id", Matchers.equalTo(id));
+    public void receberInformacoes(int id) {
+        this.response.then().body("id", equalTo(id));
     }
 
 }
